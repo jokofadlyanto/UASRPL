9dda69313cbc2bc2692a068f9939020e6c70bb65
jokofadlyanto <64948868+jokofadlyanto@users.noreply.github.com>
2021-01-23 03:05:24 -0800

    Delete home.php

--- home.php
+++
@@ -1,5 +0,0 @@
-<center><br><br><br><br><br><br><br><br>
-<img src="assets/images/logo.png" width="200px height="200px" /> <br>
-<font Size="6" face="Helvetica">Sistem Penjualan</font> <br>
-<font Size="6">Counter Handphone</font>
-</center>
